40627bd49add27bcc0aef04cbdf839ce94575aac
lzr <a78239636@163.com>
2023-11-13 08:23:13 +0800

    Modify: 基础模板变更

---
+++ template/src/Base/Main.java
@@ -0,0 +1,78 @@
+package Base;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static boolean DEBUG = false;
+    static int n, m;
+    static int[] a;
+    static List<Integer> list;
+
+    static void solve()  {
+       n = Reader.nextInt();
+    }
+    public static void main(String[] args)  {
+        Reader.init();
+        int T = Reader.nextInt();
+        while(T-- > 0) {
+            solve();
+        }
+    }
+
+
+
+    static class Reader {
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;
+
+        /**
+         * 初始化 从字节流到字符流，初始化分词器
+         */
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        /**
+         * get next word
+         */
+        static String next()  {
+            while (!tokenizer.hasMoreTokens()) {
+                //TODO add check for eof if necessary
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        static int nextInt()  {
+            return Integer.parseInt(next());
+        }
+
+        static double nextDouble()  {
+            return Double.parseDouble(next());
+        }
+    }
+    static class Pair {
+        int x, y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        static Pair MakePair(int x, int y) {
+            return new Pair(x, y);
+        }
+
+    }
+}
